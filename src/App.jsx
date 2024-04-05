23b370d38a820c989505319fd9c2da7e6d4ba4c2
Vignesh <viwanathan.vignesh@heb.com>
2024-04-05 18:57:48 +0530

    Files created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <section className="h-screen bg-green-400 ">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore
+          eum sed nemo blanditiis pariatur, repudiandae voluptas officiis quas
+          libero ut quidem reprehenderit, fugiat sit nulla quia adipisci quasi
+          perferendis corporis.
+        </p>
+      </section>
+    </div>
+  );
+};
+export default App;
