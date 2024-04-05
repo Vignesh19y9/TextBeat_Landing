7f9e85a74ae719aa317b0fc66ae52e0204fc2ba2
Vignesh <viwanathan.vignesh@heb.com>
2024-04-05 21:56:11 +0530

    Hero Section edited

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,39 +5,45 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-        VirtualR build tools
+        Turn Words into
         <span className="bg-gradient-to-r from-purple-500 to-violet-500 text-transparent bg-clip-text">
           {" "}
-          for developers
+          Stunning
+        </span>
+        Videos
+        <span className="bg-gradient-to-r from-purple-500 to-violet-500 text-transparent bg-clip-text">
+          {" "}
+          Instantly
         </span>
       </h1>
-      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-        Empower your creativity and bring your VR app ideas to life with our
-        intuitive development tools. Get started today and turn your imagination
-        into immersive reality!
+      <p className="mt-10 text-3xl text-center text-neutral-500 max-w-4xl">
+        Eliminate hours of complex video editing.
+      </p>
+      <p className="mt-0 text-3xl text-center text-neutral-500 max-w-4xl">
+        Simply Type, Preview, Export, and Share
       </p>
       <div className="flex justify-center my-10">
         <a
           href="#"
           className="bg-gradient-to-r from-purple-500 to-violet-800 py-3 px-4 mx-3 rounded-md"
         >
-          Start for free
+          Start Testing Now →
         </a>
-        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+        {/* <a href="#" className="py-3 px-4 mx-3 rounded-md border">
           Documentation
-        </a>
+        </a> */}
       </div>
       <div className="flex mt-10 justify-center">
         <video
           autoPlay
           loop
           muted
-          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
+          className="rounded-lg w-10/12 border border-purple-700 shadow-sm shadow-violet-400 mx-2 my-4"
         >
           <source src={video1} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <video
+        {/* <video
           autoPlay
           loop
           muted
@@ -45,7 +51,7 @@ const HeroSection = () => {
         >
           <source src={video2} type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
+        </video> */}
       </div>
     </div>
   );
