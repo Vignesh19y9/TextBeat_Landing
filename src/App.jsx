f17676bb6266270949509af5a27574f5461c2920
Vignesh <viwanathan.vignesh@heb.com>
2024-04-05 22:30:52 +0530

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ const App = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
-        <FeatureSection />
+        {/* <FeatureSection />
         <Workflow />
         <Pricing />
-        <Testimonials />
+        <Testimonials /> */}
         <Footer />
       </div>
     </>
