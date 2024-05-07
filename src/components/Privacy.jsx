962e7523d0e7d874b74989707086e05e37e1c9e8
Vignesh <viwanathan.vignesh@heb.com>
2024-05-07 18:18:57 +0530

    Updated Privacy & Terms

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -3,36 +3,80 @@ import React from "react";
 const Privacy = () => {
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
+      <h1 className="text-3xl font-bold mb-4 pb-10">Privacy Policy</h1>
 
-      <section>
-        <h2 className="text-2xl font-bold mb-2">1. Information Collection</h2>
+      <section className="pb-10 ">
+        <h3 className="text-xl font-bold mb-2">Last updated: May 1, 2024</h3>
         <p className="text-lg">
-          Text Beat does not collect any personal information from its users.
-          The application operates fully offline, and no data is transmitted to
-          or stored on external servers.
+          Welcome to “Text Beat”, a mobile platform that enables users to
+          create, produce and share short text videos. (the “Videos” and the
+          “Service”). We respect your privacy.
         </p>
       </section>
 
-      <section>
-        <h2 className="text-2xl font-bold mb-2">2. Log Data</h2>
+      <section className="pb-10 ">
+        <h3 className="text-xl font-bold mb-2">Overview</h3>
         <p className="text-lg">
-          Text Beat may collect certain information automatically, including the
-          type of device you use, your device's unique identifier, the IP
-          address of your device, your operating system, and other diagnostic
-          data. This information is used solely for the purpose of improving the
-          app and providing a better user experience.
+          This Privacy Policy describes how your personal information is handled
+          in Text Beat for Mac and Text Beat for iOS.
         </p>
       </section>
 
-      <section>
-        <h2 className="text-2xl font-bold mb-2">3. Third-party Services</h2>
+      <section className="pb-10 ">
+        <h3 className="text-xl font-bold mb-2">
+          We Collect No Personal Information Using Our Applications
+        </h3>
         <p className="text-lg">
-          Text Beat may display advertisements from third-party ad networks.
-          These ads may use cookies and similar technologies to personalize
-          content and ads, provide social media features, and analyze our
-          traffic. By using Text Beat, you consent to the use of these
-          technologies for advertising purposes.
+          We do not collect, use, save, or have access to any of your personal
+          data recorded in Text Beat for iOS. Individual settings relating to
+          the Text Beat app are not personal and are stored only on your device.
+          You might also be asked to provide access to your photo or music
+          library, but this is only so you can open your photos and videos in
+          Text Beat and save them back to your library with your music of
+          choice. We don’t process that information at all and have no access to
+          it.
+        </p>
+      </section>
+
+      <section className="pb-10 ">
+        <h2 className="text-xl font-bold mb-2">Analytical data</h2>
+        <p className="text-lg">
+          We may use third party analytics tools such as Google to collect
+          information about your use of the App. For example, we may record the
+          frequency and scope of your use of the Text **Beat**, the duration of
+          your sessions, the interactions you make with the Text **Beat**
+          interface, etc. This information, however, is not linked to you or
+          identified with you and is completely impersonal - we collect it for
+          statistics to improve the app interface, workflow etc. **Our
+          Applications Comply with the Children’s Online Privacy Protection
+          Act** Our applications don’t collect personal information from anyone,
+          including children under the age of 13.
+        </p>
+      </section>
+
+      <section className="pb-10 ">
+        <h2 className="text-xl font-bold mb-2">Contact Us</h2>
+        <p className="text-lg">
+          If you have any questions about this Privacy Policy, feel free to get
+          in touch with us at robzapps1@gmail.com
+        </p>
+      </section>
+
+      <section className="pb-10 ">
+        <h2 className="text-xl font-bold mb-2">
+          CHANGES TO THIS PRIVACY POLICY
+        </h2>
+        <p className="text-lg">
+          From time to time, we may change this Policy. If you are a registered
+          user, we will provide you notice of such changes through the Service
+          interface or by sending you an email notice. Your continued use of the
+          Service after the effective date of the amended Terms constitutes your
+          consent to the amended Policy. In case of legal requirement, we may
+          also introduce immediate changes to the Policy and require that you
+          accept them. In any event, if you do not consent to the amended
+          Policy, we may terminate your account and block your access to, and
+          use of, the Service. The latest version of the Policy will always be
+          accessible on our website.
         </p>
       </section>
 
