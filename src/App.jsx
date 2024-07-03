33f0e81ea4cbccac2c966ced48bef7d7c0158fbe
Vignesh <viwanathan.vignesh@heb.com>
2024-07-03 14:08:36 +0530

    index update.

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
       <Routes>
         <Route
-          path="/textbeat/"
+          path="/"
           element={
             <div className="max-w-7xl mx-auto pt-20 px-6">
               <HeroSection />
